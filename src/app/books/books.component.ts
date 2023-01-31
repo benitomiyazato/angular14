40fc7fea989684c27b7a31e884c079c71bd30539
bemiyaz <benito.miya172@gmail.com>
2023-01-31 12:36:00 -0300

    *ngFor

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 
+interface BookInterface {
+  title: string;
+  authorName: string;
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent {
-  name: string = "Terra Sonâbula";
-  author: string = "Mia Couto";
+  books : BookInterface[] = [
+    {
+      title: "Terra Sonâbula",
+      authorName: "Mia Couto",
+    },
+    {
+      title: "Vidas Secas",
+      authorName: "Graciliano Ramos"
+    }
+  ]
+
   showBooks: boolean = true;
   inputText : string | null = null;
 

e95c68e6d443665f176ca62d1995fcc9b3ecd97f
bemiyaz <benito.miya172@gmail.com>
2023-02-01 11:25:03 -0300

    bookService

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,24 +1,21 @@
-import { Component } from '@angular/core';
+import { BooksService } from './books.service';
+import { Component, OnInit } from '@angular/core';
 import { BookInterface } from '../book-interface';
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css'],
 })
-export class BooksComponent {
-  books: BookInterface[] = [
-    {
-      title: 'terra sonâbula',
-      authorName: 'mia couto',
-    },
-    {
-      title: 'vidas secas',
-      authorName: 'graciliano ramos',
-    },
-  ];
-
+export class BooksComponent implements OnInit{
+  books: BookInterface[] = [];
   showBooks: boolean = true;
   inputText: string | null = null;
+
+  constructor(private booksService : BooksService ) {}
+
+  ngOnInit(): void {
+    this.books = this.booksService.getBooks();
+  }
 
   toggleShowBooks = () => {
     this.showBooks = this.showBooks ? false : true;
